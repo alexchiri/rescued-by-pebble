c807f5488770e4df7ba9174f11fa1e978391531c
Alexandru Chirițescu <alex@alexchiri.com>
2015-02-05 21:36:57 +0100

    Small updates
    Updated README with a link to rescuetime and started to play with basic elements.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # rescued-by-pebble
-A Pebble watchface that checks your productivity levels and optionally gives you a "vibe" if you're not focused on your work.
+A Pebble watchface that checks your productivity levels using the data from [RescueTime](https://rescuetime.com) and optionally gives you a "vibe" if you're not focused on your work.
--- src/rescued-by-pebble.c
+++ src/rescued-by-pebble.c
@@ -1,48 +1,73 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
+static TextLayer *s_text_layer;
+static TextLayer *s_rescue_time_layer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    // Use 24 hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    // Use 12 hour format
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_text_layer, buffer);
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
 }
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  s_text_layer = text_layer_create((GRect) { .origin = { 0, 55 }, .size = { bounds.size.w, 42 } });
+  text_layer_set_background_color(s_text_layer, GColorClear);
+  text_layer_set_text_color(s_text_layer, GColorBlack);
+
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+
+  s_rescue_time_layer = text_layer_create((GRect) { .origin = { 0, 100 }, .size = { bounds.size.w, 50 } });
+  text_layer_set_background_color(s_rescue_time_layer, GColorClear);
+  text_layer_set_text_color(s_rescue_time_layer, GColorBlack);
+
+  text_layer_set_font(s_rescue_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_rescue_time_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_rescue_time_layer, "2");
+
+  layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_rescue_time_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+  text_layer_destroy(s_text_layer);
 }
 
 static void init(void) {
   window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
+
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
   });
   const bool animated = true;
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
   window_stack_push(window, animated);
 }
 
